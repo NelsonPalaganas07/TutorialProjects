0a9fef669601b5d420f96f0b6b233838b661533e
NelsonPalaganas07 <pyd9365@autuni.ac.nz>
2023-09-27 22:14:48 +1300

    express has setup

---
+++ MERN-tutorial/workout-app/backend/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+//creates express app
+const express = require('express');
+
+//start off the express app
+const app = express();
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+//routes
+app.get('/', (req, res) => {
+    res.json({msg: 'Welcome to the app'})
+});
+
+//listen for requests
+app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT);
+})
+
